3accdc663c88e68ba84be6e3a4b032c47da21052
Balaji Ganesan <balaji7@gmail.com>
2025-10-08 09:44:11 -0700

    [Sprint1_Task6] - Checkpoint commit

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,6 +1,8 @@
-use axum::{Router, serve::Serve};
+use axum::{Router, serve::Serve, routing::post, response::IntoResponse};
 use tower_http::services::ServeDir;
 use std::error::Error;
+use reqwest::StatusCode;
+
 pub struct Application {
     server: Serve<Router, Router>,
     pub address: String,
@@ -9,7 +11,8 @@ pub struct Application {
 impl Application {
     pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
         let router = Router::new()
-            .nest_service("/", ServeDir::new("assets"));
+            .nest_service("/", ServeDir::new("assets"))
+            .route("/signup", post(signup));
         let listener = tokio::net::TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
         let server = axum::serve(listener, router);
@@ -20,4 +23,8 @@ impl Application {
         println!("listening on {}", &self.address);
         self.server.await
     }
+}
+
+async fn signup() -> impl IntoResponse {
+    StatusCode::OK.into_response()
 }
---
+++ auth-service/tests/api/helpers.rs
@@ -0,0 +1,28 @@
+use auth_service::Application;
+
+pub struct TestApp {
+    pub address: String,
+    pub http_client: reqwest::Client,
+}
+
+impl TestApp {
+    pub async fn new() -> Self {
+        let app = Application::build("127.0.0.1:0")
+            .await
+            .expect("Failed to build test app");
+        let address = format!("http://{}", app.address.clone());
+
+        #[allow(clippy::let_underscore_drop)]
+        let _ = tokio::spawn(app.run());
+        let http_client = reqwest::Client::new();
+        Self { address, http_client }
+    }
+
+    pub async fn get_root(&self) -> reqwest::Response {
+        self.http_client
+            .get(format!("{}/", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request")
+    }
+}
